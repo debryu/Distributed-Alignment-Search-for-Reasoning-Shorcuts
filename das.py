5b28f88cf8e9a022c7e1f5829a325adaf8d334e7
debryu <nicola.debole@gmail.com>
2024-02-26 02:23:14 +0100

    all models implemented for das

--- das.py
+++ das.py
@@ -4,7 +4,7 @@ from dataset.load_ds import load_data, nMNIST
 import torch
 from argparse import Namespace
 import os
-from models.model import CUSTOM_OPERATION, ALIGNED_CUSTOM_OPERATION, ADDITION_JOINT, ADDITION_SPLIT
+from models.model import CUSTOM_OPERATION, ALIGNED_CUSTOM_OPERATION, ADDITION_JOINT, ADDITION_SPLIT, JOINT_CUSTOM_OPERATION
 from utils.iia import compute_accuracy, compute_accuracy_sum_joint, compute_accuracy_sum_split
 
 args = Namespace()
@@ -22,13 +22,16 @@ For the simple sum task, the name is:
 
 But in principle, the name can be anything for another custom dataset
 '''
-#args.file_name = 'das_dataset_interventions'
-args.file_name = 'das_summation_dataset_interventions'
+#args.file_name = 'das_summation_dataset_interventions'
+args.file_name = 'das_dataset_interventions'
+# If using the custom task, set this to True to have the "5 rule" constraint in the dataset
+args.constraint = True
+
 
 args.lr = 1e-3
-args.num_epochs = 6
-args.train_examples = 44
-args.test_examples = 10
+args.num_epochs = 5
+args.train_examples = 10000
+args.test_examples = 10000
 
 '''
 From testing, the batch size highly affects the training of the model
@@ -42,10 +45,11 @@ This is the model to choose from
 
 It has to be located inside args.model_save_path
 '''
-#args.model_name = 'das_dataset_no_interventions_best_full_aligned_architecture_t_8.pt'
-#args.model_name = 'das_summation_dataset_no_interventions_best_disentangled_290.pt'
-args.model_name = 'das_summation_dataset_no_interventions_best_entangled_460.pt'
 
+''' 
+Select the run between 'joint', 'in-between' and 'aligned'
+'''
+run = 'in-between'
 
 # This is the high level model to choose from
 # Currently used only for the custom task
@@ -77,18 +81,35 @@ def main():
     # Check whether dataset exists, if not build it
     check_dataset_interventions(n_digits, args.sequence_len, data_folder, args.file_name, dataset_dimensions, custom_task=False)
     train_ds,test_ds = load_data(data_file=args.file_name, data_folder=data_folder, args=args)
-
-    print(train_ds[0])
-
     # Load the model
-    
     if not args.custom_task:
-      #model = ADDITION_SPLIT(args = args).to(args.device)
-      model = ADDITION_JOINT(args = args).to(args.device)
+      '''
+        Choose the model (split or joint ) by uncommenting
+      '''
+      model = ADDITION_SPLIT(args = args).to(args.device)
+      args.model_name = 'sum_model_split.pt'
+      
+      ''' 
+        ______________________________________________________________________
+      '''
+
+      #model = ADDITION_JOINT(args = args).to(args.device)
+      #args.model_name = 'sum_model_joint.pt'
+      '''
+        ______________________________________________________________________
+      '''
     else:
-      # CHOOSE THE MODEL TO LOAD by uncommenting
-      #model = CUSTOM_OPERATION(args).to(args.device)
-      model = ALIGNED_CUSTOM_OPERATION(args).to(args.device)
+      # CHOOSE THE MODEL TO LOAD
+      if run == 'joint':
+        model = JOINT_CUSTOM_OPERATION(args).to(args.device)
+        args.model_name = 'das_dataset_no_interventions_best_joint_custom_12.pt' # aka joint
+      elif run == 'in-between':
+        model = CUSTOM_OPERATION(args).to(args.device)
+        args.model_name = 'das_dataset_no_interventions_best_in-between_14.pt' # aka in-between
+      elif run == 'aligned':
+        model = ALIGNED_CUSTOM_OPERATION(args).to(args.device)
+        args.model_name = 'das_dataset_no_interventions_best_full_aligned_architecture_t_8.pt' # aka aligned
+      
     model.load_state_dict(torch.load(os.path.join(args.model_save_path, args.model_name)))
     model.eval()
     train_inter_dl = torch.utils.data.DataLoader(train_ds, batch_size=args.batch_size, shuffle=True)
@@ -98,10 +119,19 @@ def main():
       targeted_layers = [ {'layer': 'l1', 'dim': 256},
                           {'layer': 'l2', 'dim': 256},
                           {'layer': 'l3', 'dim': 256} ]
+    elif model.model_type == 'joint':
+      targeted_layers = [ {'layer': 'e1', 'dim': 3584},
+                          {'layer': 'e2', 'dim': 3584},
+                          {'layer': 'e3', 'dim': 3584},
+                          {'layer': 'dc', 'dim': 256},
+                          {'layer': 'l1', 'dim': 256},
+                          {'layer': 'l2', 'dim': 256},
+                          {'layer': 'l3', 'dim': 256} ]
     elif model.model_type == 'only_sum_joint':
       targeted_layers = [ {'layer': 'e1', 'dim': 2048},
-                          {'layer': 'e2', 'dim': 1024},
-                          {'layer': 'e3', 'dim': 512},
+                          {'layer': 'e2', 'dim': 2048},
+                          {'layer': 'e3', 'dim': 2048},
+                          {'layer': 'dc', 'dim': 256},
                           {'layer': 'l1', 'dim': 256},
                           {'layer': 'l2', 'dim': 256},
                           {'layer': 'l3', 'dim': 256} ]
@@ -112,27 +142,27 @@ def main():
                          {'layer': 'd1', 'dim': 512},
                          {'layer': 'd2', 'dim': 512},
                          {'layer': 'd3', 'dim': 512},]
+      
+    interchanged_dims = [2, 16, 64, 128]
     
-
-    for targ_layer in targeted_layers:
-      if model.model_type == 'only_sum_joint':
-        rotation_model = train_rotation_sum_joint(model, train_inter_dl, args,interchange_dim=128, layer=targ_layer)
-        # Compute accuracy
-        print("Computing accuracy for layer: ", targ_layer)
-        compute_accuracy_sum_joint(model, rotation_model, test_inter_dl, targ_layer, args)
-      elif model.model_type == 'only_sum_split':
-        rotation_model = train_rotation_sum_split(model, train_inter_dl, args,interchange_dim=128, layer=targ_layer)
-        # Compute accuracy
-        print("Computing accuracy for layer: ", targ_layer)
-        compute_accuracy_sum_split(model, rotation_model, test_inter_dl, targ_layer, args)
-      else:
-        rotation_model = train_rotation(model, train_inter_dl, args,interchange_dim=64, layer=targ_layer)
-        # Compute accuracy
-        print("Computing accuracy for layer: ", targ_layer)
-        compute_accuracy(model, rotation_model, test_inter_dl, targ_layer, args)
-      
-      
-    asd
-    eval_rotation(rotation_model, model, test_inter_dl, args)
+    for i_dim in interchanged_dims:
+      for targ_layer in targeted_layers:
+        if model.model_type == 'only_sum_joint':
+          rotation_model = train_rotation_sum_joint(model, train_inter_dl, args,interchange_dim=i_dim, layer=targ_layer)
+          # Compute accuracy
+          print("Computing accuracy for layer: ", targ_layer, f'with interchange dim: {i_dim}')
+          compute_accuracy_sum_joint(model, rotation_model, test_inter_dl, targ_layer, args)
+        elif model.model_type == 'only_sum_split':
+          rotation_model = train_rotation_sum_split(model, train_inter_dl, args,interchange_dim=i_dim, layer=targ_layer)
+          # Compute accuracy
+          print("Computing accuracy for layer: ", targ_layer, f'with interchange dim: {i_dim}')
+          compute_accuracy_sum_split(model, rotation_model, test_inter_dl, targ_layer, args)
+        else:
+          rotation_model = train_rotation(model, train_inter_dl, args,interchange_dim=i_dim, layer=targ_layer)
+          # Compute accuracy
+          print("Computing accuracy for layer: ", targ_layer, f'with interchange dim: {i_dim}')
+          compute_accuracy(model, rotation_model, test_inter_dl, targ_layer, args)
+       
+    print("Done!")
 if __name__ == '__main__':
   main()
